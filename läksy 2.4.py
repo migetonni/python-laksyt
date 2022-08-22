0f06666d08c5d058ecbe130b32900920b9807852
migetonni <Mixu.Koski-Homi@metropolia.fi>
2022-08-22 14:58:19 +0300

    läksy 2.4

---
+++ läksy 2.4.py
@@ -0,0 +1,11 @@
+luku_1 = (int(input("Anna kokonaisluku: ")))
+luku_2 = (int(input("Anna kokonaisluku: ")))
+luku_3 = (int(input("Anna kokonaisluku: ")))
+
+summa = luku_1 + luku_2 + luku_3
+tulo = luku_1 * luku_2 * luku_3
+ka = summa / 3
+
+print("summa: " + str(summa))
+print("tulo: " + str(tulo))
+print("ka: " + str(ka))
