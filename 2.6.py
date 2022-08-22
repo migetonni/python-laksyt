a6b8cda20aa2db2b996c2c9a0e0a318d34c22eb4
migetonni <Mixu.Koski-Homi@metropolia.fi>
2022-08-22 15:22:07 +0300

    läksy 2.6

---
+++ 2.6.py
@@ -0,0 +1,14 @@
+import random
+x = random.randint(0, 9)
+y = random.randint(0, 9)
+z = random.randint(0, 9)
+
+print(x, y, z)
+
+w = random.randint(0, 6)
+a = random.randint(0, 6)
+s = random.randint(0, 6)
+d = random.randint(0, 6)
+
+print(w, a, s, d)
+
