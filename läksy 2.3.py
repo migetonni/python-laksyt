816fd960e79ab6d1d0c3cbadedff904b61945a79
migetonni <Mixu.Koski-Homi@metropolia.fi>
2022-08-22 14:45:27 +0300

    läksy 2.3

---
+++ läksy 2.3.py
@@ -0,0 +1,6 @@
+kanta = (float)(input("Anna neliön kanta"))
+suorakulmion_korkeus = (float)(input("Anna neliön korkeus"))
+pinta_ala = kanta * suorakulmion_korkeus
+piiri = kanta * 2 + suorakulmion_korkeus * 2
+print("suorakulmion_piiri" + str (piiri))
+print("suorakulmion_pinta-ala" + str (pinta_ala))
